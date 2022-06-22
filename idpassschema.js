f5f9e481f336088e9b5b975bb1dd0badcfa6298a
mrunal <mrunalkhade35@gmail.com>
2022-06-22 19:05:22 +0530

    make it better

--- idpassschema.js
+++
@@ -1,9 +0,0 @@
-for (const key in lecture) {
-
-    if (lecture.hasOwnProperty(key)) {
-
-        console.log(`${key}: ${lecture[key]}`);
-    }
-}
-
-link="https://stackoverflow.com/questions/40074566/how-do-i-loop-through-an-array-with-multiple-objects-and-list-certain-elements-i";
